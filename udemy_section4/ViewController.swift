b41cb519b6926e70668965b62846b2abf2120147
greedjar74 <hong7498kim@naver.com>
2022-05-09 12:54:08 +0900

    random dice

--- udemy_section4/ViewController.swift
+++ udemy_section4/ViewController.swift
@@ -8,12 +8,27 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var firstDice: UIImageView!
+    @IBOutlet weak var secondDice: UIImageView!
+    @IBOutlet weak var rollButton: UIButton!
+    
+    let imageIdx = ["", "One", "Two", "Three", "Four", "Five", "Six"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        self.configureRollButtonSetting()
     }
-
-
+    
+    @IBAction func tapRollButton(_ sender: UIButton) {
+        let random1 = Int.random(in: 1...6)
+        let random2 = Int.random(in: 1...6)
+        
+        self.firstDice.image = UIImage(named: "Dice\(imageIdx[random1]).jpg")
+        self.secondDice.image = UIImage(named: "Dice\(imageIdx[random2]).jpg")
+    }
+    
+    func configureRollButtonSetting() {
+        self.rollButton.layer.cornerRadius = 10.0
+    }
 }
 
